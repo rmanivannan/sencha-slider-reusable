3ac83a50a5d1bcc47736c2b3838a872b28663e97
rmanivannan <manivannan13tnj@gmail.com>
2014-04-12 15:30:43 +0530

    Update Main.js

--- slider-one/app/view/Main.js
+++ slider-one/app/view/Main.js
@@ -1,22 +1,32 @@
 Ext.define('Customslider.view.Main', {
-    extend: 'Ext.Container',
-    xtype: 'mainview',
-    requires: [
-		'Customslider.view.CustomSliderOne',
+  extend: 'Ext.Container',
+  xtype: 'mainview',
+  requires: [
+	  'Customslider.view.CustomSliderOne',
+  ],
+  config: {
+  	xtype:'Container',
+    cls: 'cls555',
+    scrollable: false,
+  	items:[{
+        xtype: 'titlebar',
+        title:'Sliders'
+      },{
+        xtype: 'CustomSliderOne',
+      },{
+        xtype: 'CustomSliderOne',
+        lableName:'Slider with label change',
+      },{
+        xtype: 'CustomSliderOne',
+
+        /**Initial values to setup the slider**/
+        lableName:'Slider with changes of lableName, sliderVal, minValue, maxValue, divition',
+        sliderVal: 10,
+        minValue: 0,
+        maxValue: 100,
+        divition: 2,
+      }
     ],
-    config: {
-		xtype:'Container',
-		items:[
-			{
-				xtype: 'CustomSliderOne',
-				action: 'sliderOneIndex',
-				sliderVal: 10,
-				lableName:'Slider Value',
-				minValue: 0,
-				maxValue: 100,
-				divition: 10,
-			}
-		],
 	}
 });
 
